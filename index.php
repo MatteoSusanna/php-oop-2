1627333866272311f4d1267ad7db88694b3fe761
Matteo Susanna <matteo2784@gmail.com>
2022-09-27 18:37:47 +0200

    push

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 require __DIR__ . '/class/Soldproperties.php';
 require __DIR__ . '/class/Rentproperties.php';
 
+require __DIR__ . '/class/Agent.php';
+require __DIR__ . '/class/Customer.php';
+
+$nuovoCliente = new Customer('matteo','matteo2784@gmail.com','via Roma 2');
+$nuovoAgente = new Agent('Francesco', 'fra@gmail.com', 'via Milano');
+
+$casaInVendita = new Soldproperties('70mq', 'termica', 'moderno', '100000','http://mia_foto.com');
+$casaInAffitto = new Rentproperties('40mq', 'termica', 'moderno', '500','http://mia_foto.com');
 ?>
 
 
